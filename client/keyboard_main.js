75fabde51062be07eac9d5177f12d4bcef59d88d
TATOAO <liangwentaoss@126.com>
2022-12-13 19:27:58 +0800

    modifier main.js

--- client/keyboard_main.js
+++ client/keyboard_main.js
@@ -18,6 +18,7 @@
 
 var theUrl = "http://localhost:8340/h?key="
 var theComboUrl = "http://localhost:8340/c?combo="
+var holding_keys = [];
 
 
 var mac2Win = {
@@ -56,14 +57,6 @@ var mac2Win = {
 
 
 
-// only triggered when release
-function currentKey(pressedKeys){
-	console.log("current keys being holding", pressedKeys);
-
-}
-
-
-
 // Object.keys(mac2Win).forEach((x) => {
 // 		keyboardJS.unbind(x);
 // 		keyboardJS.bind(x, (e) => {
@@ -79,20 +72,21 @@ var registed_pressed = []
 var nonuse_keys = ['windows', 'win', 'super', 'leftcommand', 'leftwindows', 'leftwin', 'leftsuper', 'mod', 'modifier', 'menu', 'spacebar', 'comma', 'period', 
 	'apostrophe', 'graveaccent', 'one', 'two', 'three', 'four', 'five', 'six', 
 	'seven', 'eight', 'nine', 'zero', 'dash', 'equal', 'equalsign', 'openbracket',
-	'closebracket', 'basckslash', 'slash', 'forwardslash', 'semicolon',
+	'closebracket', 'backslash', 'slash', 'forwardslash', 'semicolon', 'colon',
 	'exclamation', 'exclamationpoint', '!', 'at', '@', 'number', '#', 
 	'dollar', 'dollars', 'dollarsign', '$', 'percent', '%',
 	'caret', '^', 'ampersand', 'and', '&', 'asterisk', '*',
-	'openparen', '(', 'closeparen', ')', 'underscore', '_',
+	'openparen', '(', 'closeparen', ')', 'underscore', '_', ':',
 	'plus', '+', 'verticalbar', '|', 'questionmark', '?',
-	'closeanglebracket', '>'
+	'closeanglebracket', '>', 'closecurlybrace', 'closecurlybracket'
+	, '}', 'opencurlybrace', 'opencurlybracket', '{', 'quotationmark'
 ]
 // command, alt, space, enter
 
 
 function parse_nonuse_tag(listOfKeyPress){
 
-	return listOfKeyPress.filter((x) => ! (nonuse_keys.contains(x)))
+	return listOfKeyPress.filter((x) => ! (nonuse_keys.includes(x)))
 
 }
 
@@ -115,10 +109,21 @@ function pressKey(pressedKeys){
 	// console.log("just being pressed", pressedKeys);
 	const send_query = theUrl + pressedKeys.join("&key=");
 	// console.log(send_query);
-	var xmlHttp = new XMLHttpRequest();
-	console.log(pressedKeys)
-	xmlHttp.open( "GET", send_query, true ); // false for synchronous request
-  xmlHttp.send( "hhhhhh");
+	// var xmlHttp = new XMLHttpRequest();
+	// console.log(pressedKeys);
+	var current_keys =  parse_nonuse_tag(pressedKeys);
+
+	current_keys.foreach((x) => {
+
+		if (holding_keys.includes(x)) {
+			// do nothing
+		} else {
+			holding_keys.push(x);
+		}
+
+	})
+	// xmlHttp.open( "GET", send_query, true ); // false for synchronous request
+	// xmlHttp.send( "hhhhhh");
 	
 };
 
@@ -128,5 +133,6 @@ function releaseKey(pressedKeys){
 }
 
 
-keyboardJS.bind(e => currentKey(e.pressedKeys), e => currentKey(e.pressedKeys));
+keyboardJS.bind(e => pressKey(e.pressedKeys), e => pressKey(e.pressedKeys));
+// keyboardJS.bind(e => currentKey(e.pressedKeys), e => currentKey(e.pressedKeys));
 
